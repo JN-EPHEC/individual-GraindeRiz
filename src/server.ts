ffc366f925117ba2da7ad3797aa7a3da9857b88c
GraindeRiz <gregoryly45@gmail.com>
2026-02-11 18:17:59 +0200

    Commit serveur express js

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Bienvenue sur mon serveur API');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
+function greet(name: string): string{
+    return `Salut c'est ${name}`;
+}
+
+let message = greet("Greg")
+
+console.log(message)
