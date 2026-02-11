bc37bb8e82b7d8ff805ff02e261f34d56b1ef123
GraindeRiz <gregoryly45@gmail.com>
2026-02-11 18:49:44 +0200

    API etudiant

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,18 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3000;
+const etudiants = [
+{ id: 1, nom: "Dupont", prenom: "Jean" },
+{ id: 2, nom: "Martin", prenom: "Sophie" },
+{ id: 3, nom: "Doe", prenom: "John" },
+];
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Bienvenue sur mon serveur API');
+});
+
+app.get('/api/data',(req: Request, res: Response) =>{
+    res.json(etudiants)
 });
 
 app.listen(port, () => {
