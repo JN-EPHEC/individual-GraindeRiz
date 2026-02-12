9275830155c43498757147d00667ba9c58c70f09
GraindeRiz <gregoryly45@gmail.com>
2026-02-13 00:08:36 +0200

    Définition du model User

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+import { Sequelize,DataTypes } from "sequelize";
+import sequelize from '../config/database.js';
+
+
+const User = sequelize.define(
+  'User',
+  {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      // allowNull defaults to true
+    },
+  },
+  {
+    // Other model options go here
+  },
+);
+
+console.log(User === sequelize.models.User);
