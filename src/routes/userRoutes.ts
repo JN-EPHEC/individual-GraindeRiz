724f7c3e25b46a6a28359be508c4d25e3c726e88
GraindeRiz <gregoryly45@gmail.com>
2026-02-16 21:12:52 +0100

    API & routes Express (création ou amélioration d’au moins 2 endpoints).

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -11,11 +11,40 @@ router.get('/', async (req: Request, res: Response) => {
   res.json(users);
 });
 
+//GET api/users/:id
+router.get('/:id', async (req : Request, res: Response) => {
+  const id = Number(req.params.id);
+  const user = await User.findByPk(id);
+
+  return res.json(user);
+})
+
 //POST /api/users
 router.post('/', async (req: Request, res: Response) => {
   const user = await User.create(req.body);
   res.status(201).json(user);
 });
+
+//PUT /api/users/:id
+router.put('/:id', async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const { firstName, lastName } = req.body;
+
+  const user = await User.findByPk(id);
+  if (!user) {
+    return res.status(404).json({ error: "pas de user ayant cet ID" });
+  }
+
+  user.set({
+    firstName,
+    lastName,
+  });
+
+  await user.save();
+
+  return res.json(user);
+});
+
 
 //DELETE /api/users
 router.delete('/:id' ,async (req : Request, res : Response) => {
