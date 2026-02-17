e455eefc184f4d03ff516a0714984c38054b9713
GraindeRiz <gregoryly45@gmail.com>
2026-02-17 21:13:43 +0100

    Validation des entrées et gestion des erreurs (responses HTTP correctes)

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -31,42 +31,94 @@ router.get('/', async (req: Request, res: Response) => {
 
 //GET api/users/:id
 router.get('/:id', async (req : Request, res: Response) => {
-  const id = Number(req.params.id);
-  const user = await findUserById(id)
-
-  return res.json(user);
-})
+  try {
+    //récupère l'id
+    const id = Number(req.params.id);
+    //teste si id est NUMBER et >= 0
+    if (!Number.isInteger(id)|| id <= 0) {
+      return res.status(400).json({error: 'ID invalide'});
+    }
+    //Récupère le user dans la db
+    const user = await findUserById(id);
+    //teste si on a bien récupéré un USER
+    if (!user) {
+      return res.status(404).json({error: 'Pas de user ayant cet ID'});
+    }
+    //retourne le user
+    return res.json(user);
+  }
+  catch (error) {
+    //affiche l'erreur
+    console.error('Erreur:', error);
+    //retourne réponse http 500 et body erreur
+    return res.status(500).json({erreur: 'Erreur serveur'})
+  }
+});
 
 //POST /api/users
 router.post('/', async (req: Request, res: Response) => {
-  //déstructuration du req.body pour ne garder que le prénom et le nom
-  const { firstName, lastName } = req.body;
-  const user = await createUser({ firstName, lastName })
-  return res.status(201).json(user);
+  try {
+    //déstructuration du req.body pour ne garder que le prénom et le nom
+    const { firstName, lastName } = req.body;
+    const user = await createUser({ firstName, lastName })
+    return res.status(201).json(user);
+  }
+  catch (error) {
+    //affiche l'erreur
+    console.error('Erreur:', error);
+    //retourne réponse http 500 et body erreur
+    return res.status(500).json({erreur: 'Erreur serveur'})
+  }
 });
 
 //PUT /api/users/:id
 router.put('/:id', async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  //déstructuration du req.body pour ne garder que le prénom et le nom
-  const { firstName, lastName } = req.body;
-
-
-  const user = await updateUser(id,{ firstName, lastName })
-  if (!user) {
-    return res.json(user);
+  try {
+    const id = Number(req.params.id);
+    //teste si id est NUMBER et >= 0
+    if (!Number.isInteger(id) || id <= 0) {
+      return res.status(400).json({ error: 'ID invalide' });
+    }
+    //déstructuration du req.body pour ne garder que le prénom et le nom
+    const { firstName, lastName } = req.body;
+    const user = await updateUser(id,{ firstName, lastName })
+    //vérifier si on a bien récupéré un user
+    if (!user) {
+      return res.status(404).json({error: 'Pas de user ayant cet ID' });
+    }
+    return res.status(200).json(user);
+  }
+  catch (error) {
+    //affiche l'erreur
+    console.error('Erreur:', error);
+    //retourne réponse http 500 et body erreur
+    return res.status(500).json({erreur: 'Erreur serveur'})
   }
 });
 
 
 //DELETE /api/users
 router.delete('/:id' ,async (req : Request, res : Response) => {
-  const id = Number(req.params.id)
-  
-  const userDeleted = await deleteUser(id);
+  try {
+    const id = Number(req.params.id)
+    //teste si id est NUMBER et >= 0
+      if (!Number.isInteger(id) || id <= 0) {
+        return res.status(400).json({ error: 'ID invalide' });
+      }
 
-  //retourne que le user a bien été supprimé
-  return res.send(204).send('Utilisateur supprimé');
+    const userDeleted = await deleteUser(id);
+    //deleteUser retourne soit true ou false donc on peut faire un test dessus
+    if (!userDeleted) {
+        return res.status(404).json({ error: 'Pas de user ayant cet ID' });
+      }
+
+    //retourne que le user a bien été supprimé
+    return res.status(204).send('Utilisateur supprimé');
+    }
+  catch (error) {
+    console.log('Erreur:', error);
+    return res.status(500).json({errer: 'Erreur serveur' });
+  }
 })
 
 export default router;
